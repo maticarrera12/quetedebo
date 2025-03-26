d4216c29d7a5e96fb662d98c04b5a63c02247017
MatiCarrera <mcarreradev12@gmail.com>
2025-03-26 10:28:16 -0300

    nuevo footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,12 +1,43 @@
-import '../styles/FooterComponent.css'
+import "../styles/FooterComponent.css";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LanguageIcon from "@mui/icons-material/Language";
 const FooterComponent = () => {
   return (
-    <>
     <div className="footer">
-        <p>© Carrera.web 2024, all right reserved</p>
+      <div className="footer-container">
+        <div className="right">
+          <p>© Matias Carrera 2025</p>
+        </div>
+        <div className="left">
+          <a
+            href="http://www.linkedin.com/in/matias-carrera-761b45328"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-button"
+          >
+            <LinkedInIcon />
+          </a>
+          <a
+            href="https://github.com/maticarrera12"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-button"
+          >
+            <GitHubIcon />
+          </a>
+          <a
+            href="https://matiascarrera.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-button"
+          >
+            <LanguageIcon />
+          </a>
+        </div>
+      </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppQTD from './AppQTD';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <React.StrictMode>
+    <StrictMode>
       <AppQTD/>
-    </React.StrictMode>
+    </StrictMode>
   </BrowserRouter>
 );
